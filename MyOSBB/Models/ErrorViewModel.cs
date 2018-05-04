4ab88ca9954996c32782014f417da899085b526c
Vitalii Yarema <vyarema@windowslive.com>
2018-05-05 00:00:56 +0300

    Add project files.

---
+++ MyOSBB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyOSBB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
