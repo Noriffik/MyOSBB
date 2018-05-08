30c9b3e9677d62fd25d094f2aa7f55a65ab29e01
Vitalii Yarema <vyarema@windowslive.com>
2018-05-08 10:27:07 +0300

    Morning changes 8.05.2018

--- MyOSBB.DAL/Migrations/20180507221929_CorrectModels.Designer.cs
+++ MyOSBB.DAL/Migrations/20180507221929_CorrectModels.Designer.cs
@@ -8,12 +8,13 @@ using Microsoft.EntityFrameworkCore.Storage.Internal;
 using MyOSBB.DAL.Data;
 using System;
 
-namespace MyOSBB.DAL.Data.Migrations
+namespace MyOSBB.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180507221929_CorrectModels")]
+    partial class CorrectModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
